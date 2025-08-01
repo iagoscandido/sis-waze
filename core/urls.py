2a7dab98e6e797e095d5d3ac25a8fac22f104d65
iago silva <iago.scandido3@gmail.com>
2025-08-01 12:31:18 -0300

    feat: add mapas e index do waze

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from waze.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('waze/', index)  # Include the URLs from the waze app]
 ]
